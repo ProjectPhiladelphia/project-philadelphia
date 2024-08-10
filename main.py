9eefcc4d8e84f9cc9e368396c26aa89d03502cd2
Joel <joel.bermudez@iparkings.com>
2024-08-11 00:35:00 +0200

    feat(clear_request): Agregando función para limpiar la consulta

--- main.py
+++ main.py
@@ -1,10 +1,27 @@
 import uasyncio as asyncio
 
+def clear_request(request):
+    """
+    Extrae la ruta de una cadena de solicitud HTTP.
+
+    Esta función toma una cadena de solicitud HTTP en bruto y extrae el 
+    componente de la ruta de la misma. La ruta es la parte de la solicitud 
+    que sigue al método HTTP y antes de la versión HTTP.
+
+    Args:
+        request (str): La cadena de solicitud HTTP en bruto. Ejemplo:
+                       "GET /ruta/al/recurso HTTP/1.1"
+
+    Returns:
+        str: La ruta extraída de la solicitud HTTP. Ejemplo:
+             "/ruta/al/recurso"
+    """
+    return str(request).split(" ")[1]
+
 async def handle_client(reader, writer):
     # Lee la solicitud del cliente
     request = await asyncio.wait_for(reader.read(1024), timeout=5)  # Timeout de 5 segundos
-
-    print("Solicitud recibida:", request)
+    print("Solicitud recibida:", clear_request(request))
 
     # Respuesta HTTP básica
     response = """\
